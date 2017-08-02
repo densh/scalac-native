f37d9192ec6c5e7d4cb3b3b89235e456b945c8b4
Martin Duhem <martin.duhem@gmail.com>
2017-08-02 20:18:08 +0200

    ASM - ClassWriter.put

--- asm/src/main/scala/scala/tools/asm/ClassWriter.scala
+++ asm/src/main/scala/scala/tools/asm/ClassWriter.scala
@@ -780,28 +780,28 @@ class ClassWriter extends ClassVisitor(Opcodes.ASM5) {
       i
     }
 
-    private def put(i: Item): Unit = ???/*{
-        if (index + typeCount > threshold) {
-            int ll = items.length
-            int nl = ll * 2 + 1
-            Item[] newItems = new Item[nl]
-            for (int l = ll - 1 l >= 0 --l) {
-                Item j = items[l]
-                while (j != null) {
-                    int index = j.hashCode % newItems.length
-                    Item k = j.next
-                    j.next = newItems[index]
-                    newItems[index] = j
-                    j = k
-                }
-            }
-            items = newItems
-            threshold = (int) (nl * 0.75)
+    private def put(i: Item): Unit = {
+      if (this.index + typeCount > threshold) {
+        val ll = items.length
+        val nl = ll * 2 + 1
+        val newItems = new Array[Item](nl)
+        (ll - 1) to 0 by -1 foreach { l =>
+          var j = items(l)
+          while (j != null) {
+            val index = j.hashCode_ % newItems.length
+            val k = j.next
+            j.next = newItems(index)
+            newItems(index) = j
+            j = k
+          }
         }
-        int index = i.hashCode % items.length
-        i.next = items[index]
-        items[index] = i
-    }*/
+        items = newItems
+        threshold = (nl * 0.75).toInt
+      }
+      val index = i.hashCode % items.length
+      i.next = items(index)
+      items(index) = i
+    }
 
     def findItemByIndex(index: Int): Item = ???/*{
         for (Item item : items) {
