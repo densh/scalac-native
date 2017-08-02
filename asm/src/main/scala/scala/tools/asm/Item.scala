693aef9f698bf0407a920fb31db42198e0baef8a
Martin Duhem <martin.duhem@gmail.com>
2017-08-02 21:53:06 +0200

    ASM - Item.isEqualTo

--- asm/src/main/scala/scala/tools/asm/Item.scala
+++ asm/src/main/scala/scala/tools/asm/Item.scala
@@ -124,36 +124,26 @@ class Item {
         this.hashCode_ = hashCode
     }
 
-    def isEqualTo(i: Item): Boolean = ???/*{
-        switch (type) {
-        case ClassWriter.UTF8:
-        case ClassWriter.STR:
-        case ClassWriter.CLASS:
-        case ClassWriter.MTYPE:
-        case ClassWriter.TYPE_NORMAL:
-            return i.strVal1.equals(strVal1)
-        case ClassWriter.TYPE_MERGED:
-        case ClassWriter.LONG:
-        case ClassWriter.DOUBLE:
-            return i.longVal == longVal
-        case ClassWriter.INT:
-        case ClassWriter.FLOAT:
-            return i.intVal == intVal
-        case ClassWriter.TYPE_UNINIT:
-            return i.intVal == intVal && i.strVal1.equals(strVal1)
-        case ClassWriter.NAME_TYPE:
-            return i.strVal1.equals(strVal1) && i.strVal2.equals(strVal2)
-        case ClassWriter.INDY: {
-            return i.longVal == longVal && i.strVal1.equals(strVal1)
-                    && i.strVal2.equals(strVal2)
-        }
+    def isEqualTo(i: Item): Boolean = {
+      type_ match {
+        case ClassWriter.UTF8 | ClassWriter.STR | ClassWriter.CLASS | ClassWriter.MTYPE | ClassWriter.TYPE_NORMAL =>
+          i.strVal1.equals(strVal1)
+        case ClassWriter.TYPE_MERGED | ClassWriter.LONG | ClassWriter.DOUBLE =>
+          i.longVal == longVal
+        case ClassWriter.INT | ClassWriter.FLOAT =>
+          i.intVal == intVal
+        case ClassWriter.TYPE_UNINIT =>
+          i.intVal == intVal && i.strVal1.equals(strVal1)
+        case ClassWriter.NAME_TYPE =>
+          i.strVal1.equals(strVal1) && i.strVal2.equals(strVal2)
+        case ClassWriter.INDY =>
+          i.longVal == longVal && i.strVal1.equals(strVal1) && i.strVal2.equals(strVal2)
         // case ClassWriter.FIELD:
         // case ClassWriter.METH:
         // case ClassWriter.IMETH:
         // case ClassWriter.HANDLE_BASE + 1..9
-        default:
-            return i.strVal1.equals(strVal1) && i.strVal2.equals(strVal2)
-                    && i.strVal3.equals(strVal3)
-        }
-    }*/
+        case _ =>
+          i.strVal1.equals(strVal1) && i.strVal2.equals(strVal2) && i.strVal3.equals(strVal3)
+      }
+    }
 }
