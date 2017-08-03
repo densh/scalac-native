85231f155355ef4de439c0f984571f20384ec078
Martin Duhem <martin.duhem@gmail.com>
2017-08-03 13:42:02 +0200

    ASM - MethodWriter.visitInsn

--- asm/src/main/scala/scala/tools/asm/MethodWriter.scala
+++ asm/src/main/scala/scala/tools/asm/MethodWriter.scala
@@ -340,18 +340,18 @@ class MethodWriter extends MethodVisitor(Opcodes.ASM5) {
     }*/
 
     override
-    def visitInsn(opcode: Int): Unit = ???/*{
+    def visitInsn(opcode: Int): Unit = {
         lastCodeOffset = code.length
         // adds the instruction to the bytecode of the method
         code.putByte(opcode)
         // update currentBlock
         // Label currentBlock = this.currentBlock
         if (currentBlock != null) {
-            if (compute == FRAMES) {
+            if (compute == MethodWriter.FRAMES) {
                 currentBlock.frame.execute(opcode, 0, null, null)
             } else {
                 // updates current and max stack sizes
-                int size = stackSize + Frame.SIZE[opcode]
+                val size = stackSize + Frame.SIZE(opcode)
                 if (size > maxStackSize) {
                     maxStackSize = size
                 }
@@ -363,7 +363,7 @@ class MethodWriter extends MethodVisitor(Opcodes.ASM5) {
                 noSuccessor()
             }
         }
-    }*/
+    }
 
     override
     def visitIntInsn(opcode: Int, operand: Int): Unit = ???/*{
