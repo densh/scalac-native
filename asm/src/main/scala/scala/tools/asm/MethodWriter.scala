7ac8d1a21ff47fa4ee3c97b5a08ab8e0d0876de3
Martin Duhem <martin.duhem@gmail.com>
2017-08-16 14:12:50 +0200

    ASM - MethodWriter.visitSwitchInsn

--- asm/src/main/scala/scala/tools/asm/MethodWriter.scala
+++ asm/src/main/scala/scala/tools/asm/MethodWriter.scala
@@ -798,31 +798,31 @@ class MethodWriter extends MethodVisitor(Opcodes.ASM5) {
         visitSwitchInsn(dflt, labels)
     }*/
 
-    private def visitSwitchInsn(dflt: Label, labels: Array[Label]): Unit = ???/*{
+    private def visitSwitchInsn(dflt: Label, labels: Array[Label]): Unit = {
         // Label currentBlock = this.currentBlock
         if (currentBlock != null) {
-            if (compute == FRAMES) {
+            if (compute == MethodWriter.FRAMES) {
                 currentBlock.frame.execute(Opcodes.LOOKUPSWITCH, 0, null, null)
                 // adds current block successors
                 addSuccessor(Edge.NORMAL, dflt)
                 dflt.getFirst().status |= Label.TARGET
-                for (int i = 0 i < labels.length ++i) {
+                (0 until labels.length).foreach { i =>
                     addSuccessor(Edge.NORMAL, labels(i))
                     labels(i).getFirst().status |= Label.TARGET
                 }
             } else {
                 // updates current stack size (max stack size unchanged)
-                --stackSize
+                stackSize -= 1
                 // adds current block successors
                 addSuccessor(stackSize, dflt)
-                for (int i = 0 i < labels.length ++i) {
+                (0 until labels.length).foreach { i =>
                     addSuccessor(stackSize, labels(i))
                 }
             }
             // ends current block
             noSuccessor()
         }
-    }*/
+    }
 
     override
     def visitMultiANewArrayInsn(desc: String, dims: Int): Unit = ???/*{
