9a3c02c8290980ccbfb0d646adfa09bc68b92b88
Martin Duhem <martin.duhem@gmail.com>
2017-08-17 08:49:28 +0200

    Benchmarks - Import all benchmarks

---
+++ benchmarks/src/main/scala/benchmarks/Discover.scala
@@ -0,0 +1,27 @@
+package benchmarks
+object Discover {
+  val discovered: Seq[benchmarks.Benchmark[_]] = Seq(
+    // new benchmarks.DummyBenchmark,
+    // new listperm.ListPermutationsBenchmark,
+    // new deltablue.DeltaBlueBenchmark,
+    // new storage.StorageBenchmark,
+    // new sha512.SHA512Benchmark,
+    // new tracer.TracerBenchmark,
+    // new list.ListBenchmark,
+    // new havlak.HavlakBenchmark,
+    // new bounce.BounceBenchmark,
+    // new richards.RichardsBenchmark,
+    // new brainfuck.BrainfuckBenchmark,
+    // new mandelbrot.MandelbrotBenchmark,
+    // new sieve.SieveBenchmark,
+    // new gcbench.GCBenchBenchmark,
+    // new json.JsonBenchmark,
+    // new nbody.NbodyBenchmark,
+    // new queens.QueensBenchmark,
+    // new cd.CDBenchmark,
+    // new permute.PermuteBenchmark,
+    // new sudoku.SudokuBenchmark,
+    // new towers.TowersBenchmark
+  )
+}
+
